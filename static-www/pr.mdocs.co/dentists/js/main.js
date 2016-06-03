e8e29927975f53003a7181bd2c6a6a2a630e0d44
Levent Gurses <levent.gurses@64clicks.com>
2016-06-03 19:56:36 -0400

    Addded Slack integration for demo form submissions

--- static-www/pr.mdocs.co/dentists/js/main.js
+++ static-www/pr.mdocs.co/dentists/js/main.js
@@ -9,6 +9,7 @@
       var success_msg = form_btn.data('success-msg');
       var error_msg = form_btn.data('error-msg');
       var loading_msg = form_btn.data('loading-msg');
+      var form_val = JSON.stringify($(form).serializeArray());
 
       form_btn.attr("disabled","disabled");
       form_btn.text(loading_msg);
@@ -16,18 +17,33 @@
       $.ajax({
         url:'https://axc0hldmja.execute-api.us-east-1.amazonaws.com/prod/mdocsappointment',
         type: 'POST',
-        data: JSON.stringify($(form).serializeArray()),
+        data: form_val,
         contentType: 'application/json',
         dataType: 'json',
         success: function(d) {
-              form_btn.text(success_msg);
-              form_btn.addClass("success");
-              setTimeout(function() {
-                form_btn.text(button_default_msg);
-                form.reset();
-                form_btn.attr("disabled", false);
-                form_btn.removeClass("success");
-              }, 5000);
+
+          /* Slack */
+          $.ajax({
+            url:'https://hooks.slack.com/services/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',
+            type: 'POST',
+            data: JSON.stringify({"text": "NEW DEMO REQUEST!\nDETAILS: " + form_val, "username": "moneybot", "icon_url": "https://app.mdocs.co/favicon.png"}),
+            success: function(d) {
+                console.log('Succcess');
+              },
+            error: function(d) {
+                console.log('Error');
+              }
+            });
+          /* End of Slack */
+
+            form_btn.text(success_msg);
+            form_btn.addClass("success");
+            setTimeout(function() {
+              form_btn.text(button_default_msg);
+              form.reset();
+              form_btn.attr("disabled", false);
+              form_btn.removeClass("success");
+            }, 5000);
           },
           error: function(d) {
             form_btn.text(error_msg);
@@ -42,7 +58,6 @@
     }
   };
 
-  // Contact Forms
   $("#demo_form").validate(formValidateBlock);
 
 })(jQuery);
