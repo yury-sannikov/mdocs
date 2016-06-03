882cb18e138bd21a1ce38182f862a094f5194254
Levent Gurses <levent.gurses@64clicks.com>
2016-06-03 11:42:43 -0400

    Fixed JSLint warnings

--- app/src/gulpfile.js
+++ app/src/gulpfile.js
@@ -1,272 +1,274 @@
-'use strict';
+(function () {
+  'use strict';
 
-// Install: you must install gulp both globally *and* locally.
-// Make sure you `$ npm install -g gulp`
+  // Install: you must install gulp both globally *and* locally.
+  // Make sure you `$ npm install -g gulp`
 
-/**
- * Dependencies
- */
+  /**
+   * Dependencies
+   */
 
-var $             = require('gulp-load-plugins')({ lazy: true });
-var psi           = require('psi');
-var del           = require('del');
-var gulp          = require('gulp');
-var pngquant      = require('imagemin-pngquant');
-var terminus      = require('terminus');
-var runSequence   = require('run-sequence');
+  var $             = require('gulp-load-plugins')({ lazy: true });
+  var psi           = require('psi');
+  var del           = require('del');
+  var gulp          = require('gulp');
+  var pngquant      = require('imagemin-pngquant');
+  var terminus      = require('terminus');
+  var runSequence   = require('run-sequence');
 
-/**
- * Banner
- */
+  /**
+   * Banner
+   */
 
-var pkg = require('./package.json');
-var banner = [
-  '/**',
-  ' * <%= pkg.name %> - <%= pkg.description %>',
-  ' * @version v<%= pkg.version %>',
-  ' * @link <%= pkg.homepage %>',
-  ' * @license <%= pkg.licenses[0].type %>',
-  ' */',
-  ''
-].join('\n');
+  var pkg = require('./package.json');
+  var banner = [
+    '/**',
+    ' * <%= pkg.name %> - <%= pkg.description %>',
+    ' * @version v<%= pkg.version %>',
+    ' * @link <%= pkg.homepage %>',
+    ' * @license <%= pkg.licenses[0].type %>',
+    ' */',
+    ''
+  ].join('\n');
 
-/**
- * Paths
- */
+  /**
+   * Paths
+   */
 
-var paths = {
-  clean: [
-    'public/app/js/**/*.js',
-    'public/app/js/**/*.map',
-    'public/app/js/**/*.min.js',
-    'public/app/css/**/*.css',
-    'public/app/css/**/*.min.css',
-    '!public/app/js/main.js',                         // ! not
-    '!public/app/js/auth0-editprofile-widget.min.js'  // ! not
-  ],
-  js: [
-    // ============= Bootstrap  ================
-    // Enable/disable as needed but only turn on
-    // .js that is needed on *every* page. No bloat!
-    // =========================================
-    'public/app/lib/bootstrap/js/transition.js',
-    'public/app/lib/bootstrap/js/alert.js',
-    // 'public/lib/bootstrap/js/button.js',
-    // 'public/lib/bootstrap/js/carousel.js',
-    'public/app/lib/bootstrap/js/collapse.js',
-    'public/app/lib/bootstrap/js/dropdown.js',
-    // 'public/lib/bootstrap/js/modal.js',
-    'public/app/lib/bootstrap/js/tooltip.js',
-    'public/app/lib/bootstrap/js/popover.js',
-    // 'public/lib/bootstrap/js/scrollspy.js',
-    // 'public/lib/bootstrap/js/tab.js',
-    // 'public/lib/bootstrap/js/affix.js'
-    // =========================================
-    'public/app/lib/fastclick/lib/fastclick.js',
-    'public/app/js/main.js'
-  ],
-  lint: [
-    'config/**/*.js',
-    'test/**/*.js',
-    'controllers/**/*.js',
-    'models/**/*.js',
-    'app.js',
-    'app_cluster.js',
-    'gulpfile.js'
-  ],
-  less: [
-    'less/bootstrap.less',
-    'less/main.less',
-    'less/page-api.less',
-    'less/page-colors.less',
-    'less/page-dashboard.less',
-    'less/page-privacy.less',
-    'less/page-react.less'
-  ]
-};
-
-/**
- * Clean
- */
-
-gulp.task('clean', function () {
-  return del(paths.clean);
-});
-
-/**
- * Process CSS
- */
-
-gulp.task('styles', function () {
-  return gulp.src(paths.less)               // Read in Less files
-    .pipe($.less({ strictMath: true }))     // Compile Less files
-    .pipe($.autoprefixer({                  // Autoprefix for target browsers
-      browsers: ['last 2 versions'],
-      cascade: true
-    }))
-    .pipe($.csscomb())                      // Coding style formatter for CSS
-    .pipe($.csslint('.csslintrc'))          // Lint CSS
-    .pipe($.csslint.reporter())             // Report issues
-    .pipe($.rename({ suffix: '.min' }))     // Add .min suffix
-    .pipe($.csso())                         // Minify CSS
-    // .pipe($.header(banner, { pkg : pkg }))  // Add banner
-    .pipe($.size({ title: 'CSS:' }))        // What size are we at?
-    .pipe(gulp.dest('./public/app/css'))    // Save minified CSS
-    .pipe($.livereload());                // Initiate a reload
-});
-
-/**
- * Process Scripts
- */
-
-gulp.task('scripts', function () {
-  return gulp.src(paths.js)                 // Read .js files
-    .pipe($.concat(pkg.name + '.js'))       // Concatenate .js files
-    .pipe(gulp.dest('./public/app/js'))     // Save main.js here
-    .pipe($.rename({ suffix: '.min' }))     // Add .min suffix
-    .pipe($.uglify({ outSourceMap: true })) // Minify the .js
-    // .pipe($.header(banner, { pkg : pkg }))  // Add banner
-    .pipe($.size({ title: 'JS:' }))         // What size are we at?
-    .pipe(gulp.dest('./public/app/js'))     // Save minified .js
-    // .pipe($.livereload());                  // Initiate a reload
-});
-
-/**
- * Process Images
- */
-
-gulp.task('images', function () {
-  return gulp.src('images/**/*')            // Read images
-    .pipe($.changed('./public/app/img'))    // Only process new/changed
-    .pipe($.imagemin({                      // Compress images
-      progressive: true,
-      optimizationLevel: 3,
-      interlaced: true,
-      svgoPlugins: [{ removeViewBox: false }],
-      use: [pngquant()]
-    }))
-    .pipe(gulp.dest('./public/app/img'));   // Write processed images
-});
-
-/**
- * JSHint Files
- */
-
-gulp.task('lint', function () {
-  return gulp.src(paths.lint)               // Read .js files
-    .pipe($.jshint())                       // lint .js files
-    .pipe($.jshint.reporter('jshint-stylish'));
-});
-
-/**
- * JSCS Files
- */
-
-gulp.task('jscs', function () {
-  return gulp.src(paths.lint)               // Read .js files
-    .pipe($.jscs())                         // jscs .js files
-    .on('error', function (e) {
-      $.util.log(e.message);
-      $.jscs().end();
-    })
-    .pipe(terminus.devnull({ objectMode: true }));
-});
-
-/**
- * Build Task
- *   - Build all the things...
- */
-
-gulp.task('build', function (cb) {
-  runSequence(
-    'clean',                                // first clean
-    ['lint', 'jscs'],                       // then lint and jscs in parallel
-    ['styles', 'scripts', 'images'],        // etc.
-    cb);
-});
-
-/**
- * Nodemon Task
- */
-
-gulp.task('nodemon', ['build'], function (cb) {
-  $.livereload.listen();
-  var called = false;
-  $.nodemon({
-    script: 'src/index.js',
-    verbose: false,
-    env: { 'NODE_ENV': 'development', 'DEBUG': 'skeleton' },
-    // nodeArgs: ['--debug']
-    ext: 'js',
-    ignore: [
-      'gulpfile.js',
-      'public/',
-      'views/',
-      'less/',
-      'node_modules/'
+  var paths = {
+    clean: [
+      'public/app/js/**/*.js',
+      'public/app/js/**/*.map',
+      'public/app/js/**/*.min.js',
+      'public/app/css/**/*.css',
+      'public/app/css/**/*.min.css',
+      '!public/app/js/main.js',                         // ! not
+      '!public/app/js/auth0-editprofile-widget.min.js'  // ! not
+    ],
+    js: [
+      // ============= Bootstrap  ================
+      // Enable/disable as needed but only turn on
+      // .js that is needed on *every* page. No bloat!
+      // =========================================
+      'public/app/lib/bootstrap/js/transition.js',
+      'public/app/lib/bootstrap/js/alert.js',
+      // 'public/lib/bootstrap/js/button.js',
+      // 'public/lib/bootstrap/js/carousel.js',
+      'public/app/lib/bootstrap/js/collapse.js',
+      'public/app/lib/bootstrap/js/dropdown.js',
+      // 'public/lib/bootstrap/js/modal.js',
+      'public/app/lib/bootstrap/js/tooltip.js',
+      'public/app/lib/bootstrap/js/popover.js',
+      // 'public/lib/bootstrap/js/scrollspy.js',
+      // 'public/lib/bootstrap/js/tab.js',
+      // 'public/lib/bootstrap/js/affix.js'
+      // =========================================
+      'public/app/lib/fastclick/lib/fastclick.js',
+      'public/app/js/main.js'
+    ],
+    lint: [
+      'config/**/*.js',
+      'test/**/*.js',
+      'controllers/**/*.js',
+      'models/**/*.js',
+      'app.js',
+      'app_cluster.js',
+      'gulpfile.js'
+    ],
+    less: [
+      'less/bootstrap.less',
+      'less/main.less',
+      'less/page-api.less',
+      'less/page-colors.less',
+      'less/page-dashboard.less',
+      'less/page-privacy.less',
+      'less/page-react.less'
     ]
-  })
-  .on('start', function () {
-    setTimeout(function () {
-      if (!called) {
-        called = true;
-        cb();
-      }
-    }, 3030);  // wait for start
-  })
-  .on('restart', function () {
-    setTimeout(function () {
-      $.livereload.changed('/');
-    }, 3030);  // wait for restart
+  };
+
+  /**
+   * Clean
+   */
+
+  gulp.task('clean', function () {
+    return del(paths.clean);
   });
-});
 
-/**
- * Open the browser
- */
+  /**
+   * Process CSS
+   */
 
-gulp.task('open', ['nodemon'], function () {
-  gulp.src('')
-  .pipe($.open({ uri: 'http://localhost:3030' }));
-});
+  gulp.task('styles', function () {
+    return gulp.src(paths.less)               // Read in Less files
+      .pipe($.less({ strictMath: true }))     // Compile Less files
+      .pipe($.autoprefixer({                  // Autoprefix for target browsers
+        browsers: ['last 2 versions'],
+        cascade: true
+      }))
+      .pipe($.csscomb())                      // Coding style formatter for CSS
+      .pipe($.csslint('.csslintrc'))          // Lint CSS
+      .pipe($.csslint.reporter())             // Report issues
+      .pipe($.rename({ suffix: '.min' }))     // Add .min suffix
+      .pipe($.csso())                         // Minify CSS
+      // .pipe($.header(banner, { pkg : pkg }))  // Add banner
+      .pipe($.size({ title: 'CSS:' }))        // What size are we at?
+      .pipe(gulp.dest('./public/app/css'))    // Save minified CSS
+      .pipe($.livereload());                // Initiate a reload
+  });
 
-/**
- * Default Task
- */
+  /**
+   * Process Scripts
+   */
 
-gulp.task('default', ['open'], function () {
-  gulp.watch(paths.less, ['styles']);
-  gulp.watch(paths.js, ['scripts']);
-  gulp.watch(paths.lint, ['lint']);
-  gulp.watch('views/**/*.jade').on('change', $.livereload.changed);
-});
+  gulp.task('scripts', function () {
+    return gulp.src(paths.js)                 // Read .js files
+      .pipe($.concat(pkg.name + '.js'))       // Concatenate .js files
+      .pipe(gulp.dest('./public/app/js'))     // Save main.js here
+      .pipe($.rename({ suffix: '.min' }))     // Add .min suffix
+      .pipe($.uglify({ outSourceMap: true })) // Minify the .js
+      // .pipe($.header(banner, { pkg : pkg }))  // Add banner
+      .pipe($.size({ title: 'JS:' }))         // What size are we at?
+      .pipe(gulp.dest('./public/app/js'));     // Save minified .js
+      // .pipe($.livereload());                  // Initiate a reload
+  });
 
-/**
- * Run PageSpeed Insights
- */
+  /**
+   * Process Images
+   */
 
-// When using this module for a production-level build process,
-// registering for an API key from the Google Developer Console
-// is recommended.
+  gulp.task('images', function () {
+    return gulp.src('images/**/*')            // Read images
+      .pipe($.changed('./public/app/img'))    // Only process new/changed
+      .pipe($.imagemin({                      // Compress images
+        progressive: true,
+        optimizationLevel: 3,
+        interlaced: true,
+        svgoPlugins: [{ removeViewBox: false }],
+        use: [pngquant()]
+      }))
+      .pipe(gulp.dest('./public/app/img'));   // Write processed images
+  });
 
-var site = 'skeleton-app.jit.su';
+  /**
+   * JSHint Files
+   */
 
-gulp.task('mobile', function (cb) {
-  // output a formatted report to the terminal
-  psi.output(site, {
-    strategy: 'mobile',
-    locale: 'en_US',
-    threshold: 70
-  }, cb);
-});
+  gulp.task('lint', function () {
+    return gulp.src(paths.lint)               // Read .js files
+      .pipe($.jshint())                       // lint .js files
+      .pipe($.jshint.reporter('jshint-stylish'));
+  });
 
-gulp.task('desktop', ['mobile'], function (cb) {
-  // output a formatted report to the terminal
-  psi.output(site, {
-    strategy: 'desktop',
-    locale: 'en_US',
-    threshold: 80
-  }, cb);
-});
+  /**
+   * JSCS Files
+   */
 
-gulp.task('pagespeed', ['desktop']);
+  gulp.task('jscs', function () {
+    return gulp.src(paths.lint)               // Read .js files
+      .pipe($.jscs())                         // jscs .js files
+      .on('error', function (e) {
+        $.util.log(e.message);
+        $.jscs().end();
+      })
+      .pipe(terminus.devnull({ objectMode: true }));
+  });
+
+  /**
+   * Build Task
+   *   - Build all the things...
+   */
+
+  gulp.task('build', function (cb) {
+    runSequence(
+      'clean',                                // first clean
+      ['lint', 'jscs'],                       // then lint and jscs in parallel
+      ['styles', 'scripts', 'images'],        // etc.
+      cb);
+  });
+
+  /**
+   * Nodemon Task
+   */
+
+  gulp.task('nodemon', ['build'], function (cb) {
+    $.livereload.listen();
+    var called = false;
+    $.nodemon({
+      script: 'src/index.js',
+      verbose: false,
+      env: { 'NODE_ENV': 'development', 'DEBUG': 'skeleton' },
+      // nodeArgs: ['--debug']
+      ext: 'js',
+      ignore: [
+        'gulpfile.js',
+        'public/',
+        'views/',
+        'less/',
+        'node_modules/'
+      ]
+    })
+    .on('start', function () {
+      setTimeout(function () {
+        if (!called) {
+          called = true;
+          cb();
+        }
+      }, 3030);  // wait for start
+    })
+    .on('restart', function () {
+      setTimeout(function () {
+        $.livereload.changed('/');
+      }, 3030);  // wait for restart
+    });
+  });
+
+  /**
+   * Open the browser
+   */
+
+  gulp.task('open', ['nodemon'], function () {
+    gulp.src('')
+    .pipe($.open({ uri: 'http://localhost:3030' }));
+  });
+
+  /**
+   * Default Task
+   */
+
+  gulp.task('default', ['open'], function () {
+    gulp.watch(paths.less, ['styles']);
+    gulp.watch(paths.js, ['scripts']);
+    gulp.watch(paths.lint, ['lint']);
+    gulp.watch('views/**/*.jade').on('change', $.livereload.changed);
+  });
+
+  /**
+   * Run PageSpeed Insights
+   */
+
+  // When using this module for a production-level build process,
+  // registering for an API key from the Google Developer Console
+  // is recommended.
+
+  var site = 'skeleton-app.jit.su';
+
+  gulp.task('mobile', function (cb) {
+    // output a formatted report to the terminal
+    psi.output(site, {
+      strategy: 'mobile',
+      locale: 'en_US',
+      threshold: 70
+    }, cb);
+  });
+
+  gulp.task('desktop', ['mobile'], function (cb) {
+    // output a formatted report to the terminal
+    psi.output(site, {
+      strategy: 'desktop',
+      locale: 'en_US',
+      threshold: 80
+    }, cb);
+  });
+
+  gulp.task('pagespeed', ['desktop']);
+}());
