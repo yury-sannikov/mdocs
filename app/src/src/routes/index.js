86193e4c20626a9255f7e511ec1283507d85a1a7
Yuriy Sannikov <yury.sannikov@gmail.com>
2016-06-20 22:01:26 +0300

    move /reports route out of the /app/reports

--- app/src/src/routes/index.js
+++ app/src/src/routes/index.js
@@ -1,12 +1,21 @@
 'use strict';
+const mw = require('../middleware');
 const Router = require('koa-router');
 
 const router = new Router();
 
-router.use(require('./app').routes());
-
 router.get('/health_check', function*() {
   this.body = { healthy: 1};
 });
+
+const appRoutes = require('./app');
+
+// Need session & body
+appRoutes.use(mw.ensureReferer());
+
+appRoutes.use(mw.checkJWTExpiration());
+
+router.use(appRoutes.routes());
+
 
 module.exports = router;
