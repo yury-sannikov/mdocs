819b4347d5d9488a13cba4fe7788e72c5c19f938
Vladimir Corja <v.corja@yahoo.ca>
2016-06-24 14:54:43 -0400

    Added more sites to stage two (scraping ratings and reviews #)

--- tools/marketing-automation/automator/commands/getsites.js
+++ tools/marketing-automation/automator/commands/getsites.js
@@ -36,13 +36,13 @@ export function* execute(inputStream, params) {
 
 var parseHealthGrades$ = function (url) {
   var result = {
-        "siteId": "HealthGrades",
-        "siteName": "HEALTH GRADES",
-        "siteUrl": url,
-        "addressAccurate": true,
-        "reviewCount": 0,
-        "rating": 0
-      };
+    "siteId": "HealthGrades",
+    "siteName": "HEALTH GRADES",
+    "siteUrl": url,
+    "addressAccurate": true,
+    "reviewCount": 0,
+    "rating": 0
+  };
 
   return sp.load(url)
     .then(function($){
@@ -61,30 +61,163 @@ var parseHealthGrades$ = function (url) {
 
 var parseVitals$ = function (url) {
   var result = {
-        "siteId": "Vitals",
-        "siteName": "VITALS.COM",
-        "siteUrl": url,
-        "addressAccurate": true,
-        "reviewCount": 0,
-        "rating": 0
-      };
+    "siteId": "Vitals",
+    "siteName": "VITALS.COM",
+    "siteUrl": url,
+    "addressAccurate": true,
+    "reviewCount": 0,
+    "rating": 0
+  };
 
   return sp.load(url)
     .then(function($){
 
       $.q("//div[@class='child']/span[1]").forEach(function(node){
-        result.rating = parseFloat(node.textContent);
+        if(!node.getAttribute('class').contains('noreviews')) {
+          result.rating = parseFloat(node.textContent);
+        }
       });
-      $.q("//a[@class='reviews_count']").forEach(function(node){
+      $.q("//div[@class='child']/span[2]").forEach(function(node){
+        result.reviewCount = parseFloat(node.textContent.replace(/\(/g, '').replace(/\)/g, ''));
+      });
+      return result;
+    })
+    .fail(function(err){
+      console.error(err);
+    });
+}
+
+var parseYelp$ = function (url) {
+  var result = {
+    "siteId": "Yelp",
+    "siteName": "YELP.COM",
+    "siteUrl": url,
+    "addressAccurate": true,
+    "reviewCount": 0,
+    "rating": 0
+  };
+
+  return sp.load(url)
+    .then(function($){
+
+      $.q("//meta[@itemprop='ratingValue'][1]").forEach(function(node){
+        result.rating = parseFloat(node.getAttribute('content'));
+      });
+      $.q("//span[@itemprop='reviewCount'][1]").forEach(function(node){
         result.reviewCount = parseFloat(node.textContent.split(' ')[0]);
       });
       return result;
-  })
-  .fail(function(err){
-    console.error(err);
-  })
+    })
+    .fail(function(err){
+      console.error(err);
+    });
 }
 
+var parseRateMDs$ = function (url) {
+  var result = {
+    "siteId": "RateMDs",
+    "siteName": "RATEMDS.COM",
+    "siteUrl": url,
+    "addressAccurate": true,
+    "reviewCount": 0,
+    "rating": 0
+  };
+
+  return sp.load(url)
+    .then(function($){
+
+      $.q("//meta[@itemprop='ratingValue'][1]").forEach(function(node){
+        result.rating = parseFloat(node.getAttribute('content'));
+      });
+      $.q("//span[@itemprop='ratingCount'][1]").forEach(function(node){
+        result.reviewCount = parseFloat(node.textContent);
+      });
+      return result;
+    })
+    .fail(function(err){
+      console.error(err);
+    });
+}
+
+var parseYellowPages$ = function (url) {
+  var result = {
+    "siteId": "YellowPages",
+    "siteName": "YELLOWPAGES.COM",
+    "siteUrl": url,
+    "addressAccurate": true,
+    "reviewCount": 0,
+    "rating": 0
+  };
+
+  return sp.load(url)
+    .then(function($){
+
+      $.q("//meta[@itemprop='ratingValue'][1]").forEach(function(node){
+        result.rating = parseFloat(node.getAttribute('content'));
+      });
+      $.q("//span[@itemprop='reviewCount'][1]").forEach(function(node){
+        result.reviewCount = parseFloat(node.textContent.replace(/\(/g, '').replace(/\)/g, ''));
+      });
+      return result;
+    })
+    .fail(function(err){
+      console.error(err);
+    });
+}
+
+var parseMerchantCircle$ = function (url) {
+  var result = {
+    "siteId": "MerchantCircle",
+    "siteName": "MERCHANTCIRCLE.COM",
+    "siteUrl": url,
+    "addressAccurate": true,
+    "reviewCount": 0,
+    "rating": 0
+  };
+
+  return sp.load(url)
+    .then(function($){
+
+      $.q("//*[@id='business-aggregate-rating'][1]").forEach(function(node){
+        result.rating = parseFloat(node.textContent);
+      });
+      $.q("//*[@class='bCount'][1]").forEach(function(node){
+        result.reviewCount = parseFloat(node.textContent);
+      });
+      return result;
+    })
+    .fail(function(err){
+      console.error(err);
+    });
+}
+
+var parseYahooLocal$ = function (url) {
+  var result = {
+    "siteId": "Yahoo",
+    "siteName": "LOCAL.YAHOO.COM",
+    "siteUrl": url,
+    "addressAccurate": true,
+    "reviewCount": 0,
+    "rating": 0
+  };
+
+  return sp.load(url)
+    .then(function($){
+
+      $.q("//div[@class='info-rating']/span[1]").forEach(function(node){
+        result.rating = parseFloat(node.getAttribute('data-value'));
+      });
+      $.q("//div[@class='info-rating'][1]").forEach(function(node){
+        result.reviewCount = parseFloat(node.textContent.split(' ')[0]);
+      });
+      return result;
+    })
+    .fail(function(err){
+      console.error(err);
+    });
+}
+
+// Getting ratings from each site
 var index = 0;
 function* process(dentist) {
   index++;
@@ -93,6 +226,7 @@ function* process(dentist) {
 
   var initialData = {
     key: _uuid + '-' + dentist.name.split(' ')[0].toLowerCase() + '-' + dentist.name.split(' ')[1].toLowerCase().replace(',', ''),
+    "uuid": _uuid,
     "reportDate": moment().format("MM/DD/YYYY"),
     "providerName": dentist.name,
     "providerAddress": dentist.address + '<br>' + dentist.city + ', ' + dentist.state + ' ' + dentist.zip + '<br>' + dentist.phone,
@@ -105,7 +239,7 @@ function* process(dentist) {
   if (hgIndex > -1) {
     try {
         const data = yield parseHealthGrades$(dentist.sites[hgIndex].url);
-        initialData.reviews.push(data)
+        initialData.reviews.push(data);
     } catch (e) {
       console.error(e.stack);
     }
@@ -118,13 +252,79 @@ function* process(dentist) {
   if (vIndex > -1) {
     try {
         const data = yield parseVitals$(dentist.sites[vIndex].url);
-        initialData.reviews.push(data)
+        initialData.reviews.push(data);
     } catch (e) {
         console.error(e.stack);
     }
   } else {
     console.error("Vitals URL not found for record #: " + index + '   Name: ' + dentist.name);
   }
+
+  // Yelp.com
+  var vIndex = _.findLastIndex(dentist.sites, function(s) {return s.site.toLowerCase() == 'yelp';});
+  if (vIndex > -1) {
+    try {
+        const data = yield parseYelp$(dentist.sites[vIndex].url);
+        initialData.reviews.push(data);
+    } catch (e) {
+        console.error(e.stack);
+    }
+  } else {
+    console.error("Yelp URL not found for record #: " + index + '   Name: ' + dentist.name);
+  }
+
+  // RateMDs.com
+  var vIndex = _.findLastIndex(dentist.sites, function(s) {return s.site.toLowerCase() == 'ratemds';});
+  if (vIndex > -1) {
+    try {
+        const data = yield parseRateMDs$(dentist.sites[vIndex].url);
+        initialData.reviews.push(data);
+    } catch (e) {
+        console.error(e.stack);
+    }
+  } else {
+    console.error("RateMDs URL not found for record #: " + index + '   Name: ' + dentist.name);
+  }
+
+  // YellowPages.com
+  var vIndex = _.findLastIndex(dentist.sites, function(s) {return s.site.toLowerCase() == 'yellowpages';});
+  if (vIndex > -1) {
+    try {
+        const data = yield parseYellowPages$(dentist.sites[vIndex].url);
+        initialData.reviews.push(data);
+    } catch (e) {
+        console.error(e.stack);
+    }
+  } else {
+    console.error("YellowPages URL not found for record #: " + index + '   Name: ' + dentist.name);
+  }
+
+  // MerchantCircle.com
+  var vIndex = _.findLastIndex(dentist.sites, function(s) {return s.site.toLowerCase() == 'merchantcircle';});
+  if (vIndex > -1) {
+    try {
+        const data = yield parseMerchantCircle$(dentist.sites[vIndex].url);
+        initialData.reviews.push(data);
+    } catch (e) {
+        console.error(e.stack);
+    }
+  } else {
+    console.error("MerchantCircle URL not found for record #: " + index + '   Name: ' + dentist.name);
+  }
+
+  // Yahoo Local
+  var vIndex = _.findLastIndex(dentist.sites, function(s) {return s.site.toLowerCase() == 'yahoo';});
+  if (vIndex > -1) {
+    try {
+        const data = yield parseYahooLocal$(dentist.sites[vIndex].url);
+        initialData.reviews.push(data);
+    } catch (e) {
+        console.error(e.stack);
+    }
+  } else {
+    console.error("Yahoo Local URL not found for record #: " + index + '   Name: ' + dentist.name);
+  }
+
   return Object.assign({}, dentist, initialData);
 
 }
