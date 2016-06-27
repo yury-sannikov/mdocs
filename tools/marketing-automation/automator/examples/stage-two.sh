bd21a4c2aaea37ecac42932988928565c4b6b449
Vladimir Corja <v.corja@yahoo.ca>
2016-06-27 10:44:21 -0400

    Make output files more readable

--- tools/marketing-automation/automator/examples/stage-two.sh
+++ tools/marketing-automation/automator/examples/stage-two.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 set -e
 ./automator getsites --input-file ./doctors-input-s2.json --output-file ./doctors-output-s2.json --selector providers.*
+node makeReadable.js 'doctors-output-s2.json'
