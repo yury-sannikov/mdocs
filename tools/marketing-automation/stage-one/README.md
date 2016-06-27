14dfb4929f1ef2825e3ea7a30196b7b8a4157a09
Vladimir Corja <v.corja@yahoo.ca>
2016-06-27 14:31:31 -0400

    Easier set up for stage two

--- tools/marketing-automation/stage-one/README.md
+++ tools/marketing-automation/stage-one/README.md
@@ -1,2 +1,3 @@
 * Execute command: $ npm run execute
 * Address verification command: $ npm run verify
+* Copying to stage two command: $ npm run process
---
+++ tools/marketing-automation/stage-one/copyToStageTwo.js
@@ -0,0 +1,10 @@
+var fs = require('fs');
+
+var fileInputName = 'doctors.json';
+var fileOutputName = '../automator/examples/doctors-input-s2.json';
+
+var data = JSON.parse(fs.readFileSync(fileInputName, 'utf8'));
+
+fs.writeFile(fileOutputName, JSON.stringify(data, null, 2), (err) => {
+    if (err) throw err;
+  });
