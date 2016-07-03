547cbb90fbda4a8b300c159f2c07a0ca5992a2bb
Levent Gurses <levent.gurses@64clicks.com>
2016-07-02 20:21:05 -0400

    Dashboard design

--- static-www/app.mdocs.co/dash/ng/assets/js/controllers.js
+++ static-www/app.mdocs.co/dash/ng/assets/js/controllers.js
@@ -12,49 +12,46 @@ App.controller('DashboardCtrl', ['$scope', '$localStorage', '$window',
          * Init Chart.js Chart, for more examples you can check out http://www.chartjs.org/docs
          */
 
-        // Get Chart Container
-        var dashChartLinesCon  = jQuery('.js-dash-chartjs-lines')[0].getContext('2d');
+        var initChartsChartJS = function () {
+            // Get Chart Containers
+            var chartBarsCon   = jQuery('.js-chartjs-bars')[0].getContext('2d');
 
-        // Set Chart and Chart Data variables
-        var dashChartLines, dashChartLinesData;
+            // Set Chart and Chart Data variables
+            var chartBars;
 
-        // Lines Chart Data
-        var dashChartLinesData = {
-            labels: ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN'],
-            datasets: [
-                {
-                    label: 'This Week',
-                    fillColor: 'rgba(44, 52, 63, .07)',
-                    strokeColor: 'rgba(44, 52, 63, .25)',
-                    pointColor: 'rgba(44, 52, 63, .25)',
-                    pointStrokeColor: '#fff',
-                    pointHighlightFill: '#fff',
-                    pointHighlightStroke: 'rgba(44, 52, 63, 1)',
-                    data: [34, 42, 40, 65, 48, 56, 80]
-                },
-                {
-                    label: 'Last Week',
-                    fillColor: 'rgba(44, 52, 63, .1)',
-                    strokeColor: 'rgba(44, 52, 63, .55)',
-                    pointColor: 'rgba(44, 52, 63, .55)',
-                    pointStrokeColor: '#fff',
-                    pointHighlightFill: '#fff',
-                    pointHighlightStroke: 'rgba(44, 52, 63, 1)',
-                    data: [18, 19, 20, 35, 23, 28, 50]
-                }
-            ]
+            // Set global chart options
+            var globalOptions = {
+                scaleFontFamily: "'Open Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif",
+                scaleFontColor: '#999',
+                scaleFontStyle: '600',
+                tooltipTitleFontFamily: "'Open Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif",
+                tooltipCornerRadius: 3,
+                maintainAspectRatio: false,
+                responsive: true
+            };
+
+            // Lines/Bar/Radar Chart Data
+            var chartLinesBarsRadarData = {
+                labels: ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN'],
+                datasets: [
+                    {
+                        label: 'This Week',
+                        fillColor: 'rgba(171, 227, 125, .3)',
+                        strokeColor: 'rgba(171, 227, 125, 1)',
+                        pointColor: 'rgba(171, 227, 125, 1)',
+                        pointStrokeColor: '#fff',
+                        pointHighlightFill: '#fff',
+                        pointHighlightStroke: 'rgba(171, 227, 125, 1)',
+                        data: [15, 16, 20, 25, 23, 25, 32]
+                    }
+                ]
+            };
+
+            // Init Charts
+            chartBars  = new Chart(chartBarsCon).Bar(chartLinesBarsRadarData, globalOptions);
         };
 
-        // Init Lines Chart
-        dashChartLines = new Chart(dashChartLinesCon).Line(dashChartLinesData, {
-            scaleFontFamily: "'Open Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif",
-            scaleFontColor: '#999',
-            scaleFontStyle: '600',
-            tooltipTitleFontFamily: "'Open Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif",
-            tooltipCornerRadius: 3,
-            maintainAspectRatio: false,
-            responsive: true
-        });
+        initChartsChartJS();
     }
 ]);
 
