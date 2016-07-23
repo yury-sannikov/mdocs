2679a0456932ff9072e21bb92c304ebc26e8ed50
Levent Gurses <levent@movel.co>
2016-07-23 17:23:38 -0400

    Contact form partial and contact page. Closes #37

--- static-www/provider-site-metalsmith/themes/cleanui/assets/js/app.js
+++ static-www/provider-site-metalsmith/themes/cleanui/assets/js/app.js
@@ -440,6 +440,67 @@ MDOCS.swingToHash = function() {
 $(document).ready(function() {
     MDOCS.initModule();
     // MDOCS.launchCal = parseInt(getUrlParameter('book', false));
+
+    var formValidateBlock = {
+      submitHandler: function(form) {
+        console.log('I am being submitted...');
+        var form_btn = $(form).find('button[type="submit"]');
+        var button_default_msg = "Request Now";
+        var success_msg = form_btn.data('success-msg');
+        var error_msg = form_btn.data('error-msg');
+        var loading_msg = form_btn.data('loading-msg');
+        var form_val = JSON.stringify($(form).serializeArray(), null, 2);
+
+        form_btn.attr("disabled","disabled");
+        form_btn.text(loading_msg);
+
+        $.ajax({
+          url:'https://axc0hldmja.execute-api.us-east-1.amazonaws.com/prod/mdocsappointment',
+          type: 'POST',
+          data: form_val,
+          contentType: 'application/json',
+          dataType: 'json',
+          success: function(d) {
+
+            /* Slack */
+            // $.ajax({
+            //   url: slack_url,
+            //   type: 'POST',
+            //   data: JSON.stringify({"text": "NEW DEMO REQUEST!\nDETAILS: " + form_val, "username": "moneybot", "icon_url": "https://app.mdocs.co/favicon.png"}),
+            //   success: function(d) {
+            //       console.log('Succcess');
+            //     },
+            //   error: function(d) {
+            //       console.log('Error');
+            //     }
+            //   });
+            /* End of Slack */
+
+              form_btn.text(success_msg);
+              form_btn.addClass("success");
+              setTimeout(function() {
+                form_btn.text(button_default_msg);
+                form.reset();
+                form_btn.attr("disabled", false);
+                form_btn.removeClass("success");
+              }, 5000);
+            },
+            error: function(d) {
+              form_btn.text(error_msg);
+              form_btn.addClass("error");
+              setTimeout(function() {
+                form_btn.text(button_default_msg);
+                form_btn.attr("disabled", false);
+                form_btn.removeClass("error");
+              }, 5000);
+            }
+        });
+      }
+    };
+
+    // Contact Form
+    $("#contact_form").validate(formValidateBlock);
+
 });
 
 
