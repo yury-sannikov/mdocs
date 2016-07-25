f7232a82290a406a085fd1192aed90eeaea6b223
Yuriy Sannikov <yury.sannikov@gmail.com>
2016-07-25 18:25:33 +0300

    Add navigation breadcrumbs

--- app/src/src/routes/sitebuilder/index.js
+++ app/src/src/routes/sitebuilder/index.js
@@ -17,7 +17,7 @@ router.get('main', '/', function*() {
   }), true);
 });
 
-router.get('/content/:key', function*() {
+router.get('contentList', '/content/:key', function*() {
 
   if (!this.sbMetainfo.hasOwnProperty(this.params.key)) {
     this.redirect(router.url('main'))
@@ -29,7 +29,9 @@ router.get('/content/:key', function*() {
     contentItems,
     metainfo: data.metainfo,
     contentKey: this.params.key,
-    isContentOpen: true
+    isContentOpen: true,
+    nav_title: data.metainfo.menuCaption,
+    nav_crumbs: [[data.metainfo.menuCaption], ['Overview', router.url('main')]]
   }), true);
 });
 
@@ -41,10 +43,15 @@ router.get('/content/:key/:index', function*() {
   }
   const data = this.sbMetainfo[this.params.key]
   const contentItems = yield Repo.readJSONData('liberty-laser-eye-0', this.params.key)
+  const dataItem = contentItems[this.params.index];
+  const title = data.metainfo.schema.title || `${dataItem.title} Editor`
   this.render('sitebuilder/contentEditor', Object.assign({}, this.jadeLocals, {
-    page: JSON.stringify(contentItems[this.params.index]),
+    page: JSON.stringify(dataItem),
     schema: JSON.stringify(data.metainfo.schema),
-    isContentOpen: true
+    isContentOpen: true,
+    nav_title: title,
+    nav_crumbs: [[title], [data.metainfo.menuCaption, router.url('contentList', {key:this.params.key})], ['Overview', router.url('main')]]
+
   }), true);
 });
 
