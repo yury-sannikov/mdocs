867ec9b203a459b17254d6f79c00f88aa6d79199
Levent Gurses <levent@movel.co>
2016-08-03 19:23:11 -0400

    Leads CRUD API

---
+++ tools/marketing-automation/stage-one/app/routes.js
@@ -0,0 +1,86 @@
+var express     = require('express');
+var router      = express.Router();
+var Lead        = require('./models/lead');
+
+// Router middleware
+router.use(function(req, res, next) {
+  console.log('Routing in progress...');
+  next();
+});
+
+router.get('/', function(req, res) {
+  res.json({ message: 'MDOCS Marketing API' });
+});
+
+router.route('/leads')
+  .post(function(req, res) {
+
+    var lead = new Lead();
+    lead.name = req.body.name;
+    lead.email = req.body.email;
+
+    lead.save(function(err) {
+      if (err) {
+        res.send(err);
+      }
+
+      res.json({ message: 'Lead created successfully.'});
+    })
+  })
+  
+  .get(function(req, res) {
+
+    Lead.find(function(err, leads) {
+      if (err) {
+        res.send(err);
+      }
+
+      res.json(leads);
+  });
+});
+
+router.route('/leads/:lead_id')
+  .get(function(req, res) {
+    Lead.findById(req.params.lead_id, function(err, lead) {
+      if (err) {
+        res.send(err);
+      }
+
+      res.json(lead);
+    });
+  })
+
+  .put(function(req, res) {
+    Lead.findById(req.params.lead_id, function(err, lead) {
+      if (err) {
+        res.send(err);
+      }
+
+      lead.name = req.body.name;
+      lead.email = req.body.email;
+
+      lead.save(function(err) {
+        if (err) {
+          res.send(err);
+        }
+
+        res.json({ message: 'Lead updated successfully.'});
+      })
+    })
+  })
+
+  .delete(function(req, res) {
+    Lead.remove({
+      _id: req.params.lead_id
+    }, function(err, lead) {
+      if (err) {
+        res.send(err);
+      }
+
+      res.json({ message: 'Lead deleted successfully.'});
+    })
+  })
+;
+
+
+module.exports = router;
--- tools/marketing-automation/stage-one/server.js
+++ tools/marketing-automation/stage-one/server.js
@@ -5,52 +5,12 @@ var app         = express();
 var bodyParser  = require('body-parser');
 var mongoose    = require('mongoose');
 var q           = require('q');
-var Lead        = require('./app/models/lead');
-// var routes      = require('./app/routes');
+var router      = require('./app/routes');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;
-var router = express.Router();
-
-// Router middleware
-router.use(function(req, res, next) {
-  console.log('Routing in progress...');
-  next();
-});
-
-router.get('/', function(req, res) {
-  res.json({ message: 'MDOCS Marketing API' });
-});
-
-router.route('/leads')
-  .post(function(req, res) {
-
-    var lead = new Lead();
-    lead.name = req.body.name;
-    lead.email = req.body.email;
-
-    lead.save(function(err) {
-      if (err) {
-        res.send(err);
-      }
-
-      res.json({ message: 'Lead created successfully.'});
-    })
-  })
-  
-  .get(function(req, res) {
-
-    Lead.find(function(err, leads) {
-      if (err) {
-        res.send(err);
-      }
-
-      res.json(leads);
-    });
-
-  });
 
 mongoose.Promise = q.Promise;
 mongoose.connect('mongodb://root:Movel1Movel@ds139675.mlab.com:39675/mdocs-marketing');
