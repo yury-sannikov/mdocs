992b69f53037968399d1a76720efb464a9c31ff5
Yuriy Sannikov <yury.sannikov@gmail.com>
2016-09-25 12:22:45 -0400

    enable s3fuse cache to improve sitebuilder build speed

--- base-images/node-alpine/onbuild/docker-entrypoint.sh
+++ base-images/node-alpine/onbuild/docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -o errexit
 set -o errtrace
 
 rm -rf /sitebuilder-src
-mkdir -p /sitebuilder-src
-/usr/bin/s3fs mdocs-sitebuilder /sitebuilder-src -o iam_role=mdocs-apps-ecs-docker-instance-role,nosuid,nonempty,nodev,allow_other,default_acl=private,retries=5
+mkdir -p /sitebuilder-src /s3cache
+/usr/bin/s3fs mdocs-sitebuilder /sitebuilder-src -ouse_cache=/s3cache -o iam_role=mdocs-apps-ecs-docker-instance-role,nosuid,nonempty,nodev,allow_other,default_acl=private,retries=5
 
 exec "$@"
