27fc08237a4f30a72c74378665c7f7bb21c2b51f
Yuriy Sannikov <yury.sannikov@gmail.com>
2016-09-29 21:47:05 -0400

    make publish script

--- static-www/vgucc-new-structure/develop.js
+++ static-www/vgucc-new-structure/develop.js
@@ -1,6 +1,6 @@
 const path = require('path')
 const SiteBuilderEngine = require('./engine')
-  
+
 const THEME = 'cleanui'
 const WORK_DIR = path.join(__dirname, 'content')
 const BUILD_DIR = path.join(__dirname, '__build')
@@ -15,13 +15,27 @@ const options = {
 }
 let engine = new SiteBuilderEngine(WORK_DIR, BUILD_DIR, options)
 
+const isPublish = () => {
+  return (process.argv.length >= 3) && process.argv[2] === 'publish'
+}
+
+if (isPublish()) {
+
+  engine.publish({}, () => {
+    console.log('Done')
+  })
+
+  return
+}
+
+
 engine.cliDev(8080, (err, files) => {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      let filenames = Object.keys(files).join(', ');
-      console.log('>>')
-      console.log(`Updated: ${filenames}`)
-      console.log('<<')
-    })
+  if (err) {
+    console.log(err);
+    return;
+  }
+  let filenames = Object.keys(files).join(', ');
+  console.log('>>')
+  console.log(`Updated: ${filenames}`)
+  console.log('<<')
+})
