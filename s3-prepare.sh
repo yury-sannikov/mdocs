5467892644efa625335734566003c7f10df554a9
Yuriy Sannikov <yury.sannikov@gmail.com>
2016-09-30 16:33:12 -0400

    update folder structure

--- s3-prepare.sh
+++ s3-prepare.sh
@@ -5,8 +5,8 @@ set -o errexit
 set -o errtrace
 mkdir -p mdocs-sitebuilder-s3
 AWS_ACCESS_KEY_ID=AKIAJYVOLGU7FGP6LUHQ AWS_SECRET_ACCESS_KEY=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 aws s3 cp s3://mdocs-sitebuilder ./mdocs-sitebuilder-s3 --recursive
-PWD=$(echo `pwd -P`"/mdocs-sitebuilder-s3" | sed 's/\//\\\//g')
+PWD=$(echo `pwd -P`"/tmp/mdocs-sitebuilder-s3" | sed 's/\//\\\//g')
 SEDCMD=s/SITEBUILDER_SOURCE_DIR=.*/SITEBUILDER_SOURCE_DIR=${PWD}/g
-cp src/.env src/.env_backup
-sed "$SEDCMD" ./src/.env > ./src/.env.tmp
-mv ./src/.env.tmp ./src/.env
+cp app/.env app/.env_backup
+sed "$SEDCMD" ./app/.env > ./app/.env.tmp
+mv ./app/.env.tmp ./app/.env
