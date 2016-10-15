29488647541d231ed5071ddcbdc74a3e360499c8
Yuriy Sannikov <yury.sannikov@gmail.com>
2016-10-15 12:33:09 -0400

    add calendar to cfim template

--- static-www/sitebuilder-themes/cfim/assets/js/app.js
+++ static-www/sitebuilder-themes/cfim/assets/js/app.js
@@ -1252,86 +1252,57 @@ $(document).ready(function() {
     }
 
     var formValidateBlock = {
-      submitHandler: function(form) {
-        var form_btn = $(form).find('button[type="submit"]');
-        var button_default_msg = "Request Now";
-        var success_msg = form_btn.data('success-msg');
-        var error_msg = form_btn.data('error-msg');
-        var loading_msg = form_btn.data('loading-msg');
-        var formType = form_btn.data('form-type');
-        var form_val = JSON.stringify($(form).serializeArray(), null, 2);
-
-
-        form_btn.attr("disabled", "disabled");
-        form_btn.text(loading_msg);
-
-        if (formType === 'book') {
-            submitAppointment(form);
-            return
+      rules: {
+        practiceId: {
+            required: true
         }
-
-        $.ajax({
-          url:'https://axc0hldmja.execute-api.us-east-1.amazonaws.com/prod/mdocsappointment',
-          type: 'POST',
-          data: form_val,
-          contentType: 'application/json',
-          dataType: 'json',
-          success: function(d) {
-
-            /* Slack */
-            // $.ajax({
-            //   url: slack_url,
-            //   type: 'POST',
-            //   data: JSON.stringify({"text": "NEW DEMO REQUEST!\nDETAILS: " + form_val, "username": "moneybot", "icon_url": "https://app.mdocs.co/favicon.png"}),
-            //   success: function(d) {
-            //       console.log('Succcess');
-            //     },
-            //   error: function(d) {
-            //       console.log('Error');
-            //     }
-            //   });
-            /* End of Slack */
-
-              form_btn.text(success_msg);
-              form_btn.addClass("success");
-              setTimeout(function() {
-                form_btn.text(button_default_msg);
-                form.reset();
-                form_btn.attr("disabled", false);
-                form_btn.removeClass("success");
-                if (formType == 'book') {
-                  hideCalModal();
-                  showThankYouModal();
-                } else if (formType == 'review') {
-                  var rating = $(form).find('input[type="hidden"]');
-                  hideReviewModal();
-                  if (rating.val() > 3) {
-                    // Positive review
-                    showReviewThankYouModal();
-                    $('#positive-section').css('visibility', 'visible');
-                  } else {
-                    // Negative review
-                    showReviewThankYouModal();
-                    $('#positive-section').css('visibility', 'hidden');
-                  }
-
-                }
-              }, 2000);
-            },
-            error: function(d) {
-              form_btn.text(error_msg);
-              form_btn.addClass("error");
-              setTimeout(function() {
-                form_btn.text(button_default_msg);
-                form_btn.attr("disabled", false);
-                form_btn.removeClass("error");
-              }, 2000);
-            }
-        });
-      OC}
+      }
     };
 
     // Contact Form
     $(".contact_form").validate(formValidateBlock);
     $(".review_form").validate(formValidateBlock);
+    $("#new-patient-yes").click(function() {
+        $('input.js-bookonline[name="isnew"]').val('yes');
+    })
+    $("#new-patient-no").click(function() {
+        $('input.js-bookonline[name="isnew"]').val('');
+    })
 });
+
+// Contact Form Global fns
+function contactBeforeSubmitCallback() {
+    var form_btn = $(this).find('button[type="submit"]');
+    var loading_msg = form_btn.data('loading-msg');
+    form_btn.attr("disabled", "disabled");
+    form_btn.text(loading_msg);
+}
+
+function contactSubmitCallback(maybeError) {
+    var form = $(this);
+    var form_btn = form.find('button[type="submit"]');
+    var success_msg = form_btn.data('success-msg');
+    var error_msg = form_btn.data('error-msg');
+    var button_default_msg = "Request Now";
+
+    setTimeout(function() {
+        form_btn.text(button_default_msg);
+        form_btn.attr("disabled", false);
+        form_btn.removeClass("error");
+        form_btn.addClass("success");
+    }, 2000);
+
+    if (maybeError) {
+        form_btn.text(error_msg);
+        form_btn.addClass("error");
+    } else {
+        form_btn.text(success_msg);
+        form_btn.addClass("success");
+        setTimeout(function() {
+            form[0].reset();
+            hideCalModal();
+            showThankYouModal();
+        }, 2000);
+
+    }
+}
