18e6f35c62b8189f9cfa96a55d1e590aa7ea4a74
Yury Sannikov <yury.sannikov@gmail.com>
2018-05-16 21:02:20 -0400

    use different ECR & docker updates/login

--- base-images/node-alpine/onbuild/docker-entrypoint.sh
+++ base-images/node-alpine/onbuild/docker-entrypoint.sh
@@ -5,6 +5,6 @@ set -o errtrace
 
 rm -rf /sitebuilder-src
 mkdir -p /sitebuilder-src /s3cache
-/usr/bin/s3fs mdocs-sitebuilder /sitebuilder-src -ouse_cache=/s3cache -o iam_role=mdocs-apps-ecs-docker-instance-role,nosuid,nonempty,nodev,allow_other,default_acl=private,retries=5
+# /usr/bin/s3fs mdocs-sitebuilder /sitebuilder-src -ouse_cache=/s3cache -o iam_role=mdocs-apps-ecs-docker-instance-role,nosuid,nonempty,nodev,allow_other,default_acl=private,retries=5
 
 exec "$@"
